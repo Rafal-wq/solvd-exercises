af1fdb6ee80564ceb5d68e7f20de263d5b55f0c7
Rafal-wq <rafal_wilczewski@wp.pl>
2023-02-07 19:06:37 +0100

    Create debounce function

---
+++ functionDebounce.js
@@ -0,0 +1,19 @@
+function debounce(func, wait) {
+    let timeout
+    return function(...args) {
+        const context = this
+        clearTimeout(timeout)
+        timeout = setTimeout(() => func.apply(context, args), wait)
+    }
+}
+
+function sayHello() {
+    console.log('Hello everyone, my name is', this.name)
+}
+
+const rafael = {
+    name: 'rafael',
+    speak: debounce(sayHello, 0),
+}
+
+rafael.speak()
