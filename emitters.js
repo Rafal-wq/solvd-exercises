e0554671c902b01221c34152b58fc4d3049bdbeb
Rafal-wq <rafal_wilczewski@wp.pl>
2023-02-20 19:50:50 +0100

    Adding arguments to emit process and put listeners into variables

--- emitters.js
+++ emitters.js
@@ -2,33 +2,42 @@ import { EventEmitter } from 'node:events';
 
 const myEE = new EventEmitter();
 
-myEE.on('event1', () => console.log('event 1'));
-myEE.on('event2', () => console.log('event 2'));
-myEE.on('event3', () => console.log('event 3'));
-myEE.prependListener('event6', () => console.log('event6'));
-myEE.prependListener('event5', () => console.log('event5'));
-myEE.prependListener('event4', () => console.log('event4'));
+const callbackOn1 = () => console.log(`Hello from eventOn !`);
+myEE.on('eventOn', callbackOn1);
 
-myEE.emit('event1');
-myEE.emit('event1');
-myEE.emit('event2');
-myEE.emit('event2');
-myEE.emit('event2');
-myEE.emit('event3');
-myEE.emit('event3');
-myEE.emit('event4');
-myEE.emit('event5');
-myEE.emit('event6');
-myEE.emit('event6');
-myEE.emit('event6');
-myEE.emit('event6');
+const callbackOn2 = (arg1, arg2) =>
+    console.log(`2 params from eventOn ${arg1}, ${arg2}`);
+myEE.on('eventOn', callbackOn2);
 
-myEE.removeListener('event1', () => console.log('event 1'));
-myEE.removeListener('event1', () => console.log('event 1'));
-myEE.removeListener('event1', () => console.log('event 1'));
-myEE.removeListener('event1', () => console.log('event 1'));
-myEE.removeListener('event1', () => console.log('event 1'));
-myEE.removeListener('event1', () => console.log('event 1'));
+const callbackOn3 = (...args) => {
+    const params = args.join(', ');
+    console.log(`All params from eventOn ${params}`)
+};
+myEE.on('eventOn', callbackOn3);
+
+const callbackPrep1 = () => console.log(`Hello from eventPrep !`);
+myEE.prependListener('eventPrep', callbackPrep1);
+
+const callbackPrep2 = (arg1, arg2) =>
+    console.log(`2 params from eventPrep ${arg1}, ${arg2}`);
+myEE.prependListener('eventPrep', callbackPrep2);
+
+const callbackPrep3 = (...args) => {
+    const params = args.join(', ');
+    console.log(`All params from eventPrep ${params}`)
+};
+myEE.prependListener('eventPrep', callbackPrep3);
+
+
+myEE.emit('eventPrep', 7, 8, 9, 10);
+myEE.emit('eventOn', 1, 2, 3, 4, 5, 6);
+
+myEE.removeListener('eventOn', callbackOn1);
+myEE.removeListener('eventOn', callbackOn2);
+myEE.removeListener('eventOn', callbackOn3);
+myEE.removeListener('eventPrep', callbackPrep1);
+myEE.removeListener('eventPrep', callbackPrep2);
+myEE.removeListener('eventPrep', callbackPrep3);
 
 // You need to have this script in your package.json :
 // "main": "index.js", because of import method.
