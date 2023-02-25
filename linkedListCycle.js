1d00332a421db9dfb5bf8f33284f82fc7cad6bb6
Rafal-wq <rafal_wilczewski@wp.pl>
2023-02-25 22:24:26 +0100

    Adding methods of adding, removing and printing all linked list

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,18 +1,65 @@
-const list = {
-    head: {
-        value: 6,
-        next: {
-            value: 10,
-            next: {
-                value: 12,
-                next: {
-                    value: 3,
-                    next: null
-                }
-            }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    add(value) {
+        this.length++;
+        let newNode = new Node(value);
+
+        if (this.tail) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+            return newNode;
+        }
+
+        this.head = this.tail = newNode;
+        return newNode;
+    }
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
         }
     }
-};
+    removeHead() {
+        if (this.head) {
+            this.length--;
+            const removedNode = this.head;
+            this.head = this.head.next;
+            return removedNode;
+        }
+        return undefined;
+    }
+    removeIndex(index) {
+        if (index >= this.length) {
+            throw new Error("Remove index out of bounds");
+        }
+
+        if (index === 0) {
+            return this.removeHead();
+        }
+
+        this.length--;
+        let previousNode = null;
+        let currentNode = this.head;
+        for (let i = 0; i < index; i++) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        previousNode.next = currentNode.next;
+        return currentNode;
+    }
+}
 
 const hasCycle = function(head) {
     if (head == null) return false;
@@ -25,4 +72,14 @@ const hasCycle = function(head) {
     return true;
 };
 
-console.log(hasCycle(list));
+const linkedList = new LinkedList();
+
+linkedList.add(7);
+linkedList.add(8);
+linkedList.add(9);
+linkedList.add(10);
+linkedList.print();
+linkedList.removeIndex(2);
+linkedList.print();
+
+console.log(hasCycle(linkedList));
